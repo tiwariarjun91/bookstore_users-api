80e382b03232625cb87c54bd9a13e90b856cfcc4
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-04-12 23:58:37 +0530

    Started the Server on localhost/8080

--- app/application.go
+++ app/application.go
@@ -1,5 +1,16 @@
 package app
 
+import(
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	router = gin.Default()
+)
+
 func StartApplication(){
+
+	mapUrls()
+	router.Run()
 
 }
--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -1,1 +1,7 @@
 package controllers
+
+func CreateUser(){}
+
+func GetUser(){}
+
+func FindUser(){}
